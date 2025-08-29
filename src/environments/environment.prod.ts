ece392500f64aefe020561d098b64d77faa712f0
krishh26 <kishansinhparmar999@gmail.com>
2025-08-29 15:30:16 +0530

    changes done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
 	production: false,
 
 	url: "http://localhost:4200",
-	 baseUrl : 'http://localhost:3000/api/v1',
+	 baseUrl : 'http://localhost:3100/api/v1',
 	// baseUrl: 'https://api.saivensolutions.co.uk/api/v1',
 	recaptcha: {
 		siteKey: '6Ld9_jcrAAAAAEpv7ImSiAiLIdjRcmx8q7IIZj2H',
