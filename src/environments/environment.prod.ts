d67a90c826216cb518183e49b7ba2750c5474568
darshan <darshandumaraliya@gmail.com>
2025-09-06 02:15:21 +0530

    changes done.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,9 +2,9 @@ export const environment = {
 	production: false,
 
 	url: "http://localhost:4200",
-	//  baseUrl : 'http://localhost:3100/api/v1',
-	baseUrl: 'https://api.saivensolutions.co.uk/api/v1',
+	// baseUrl: 'http://localhost:3100/api/v1',
+	baseUrl: 'https://newapi.saivensolutions.co.uk/api/v1',
 	recaptcha: {
 		siteKey: '6Ld9_jcrAAAAAEpv7ImSiAiLIdjRcmx8q7IIZj2H',
-	  },
-  };
+	},
+};
